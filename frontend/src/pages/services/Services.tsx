83579619941a08f4db22ba08f4065e9632e5fc48
jlaranjeira <jlaranjeira@hotmail.com>
2023-10-16 10:48:01 -0300

    Criação da página de serviços

--- frontend/src/pages/services/Services.tsx
+++ frontend/src/pages/services/Services.tsx
@@ -1,9 +1,64 @@
-import "./services.scss"
+import { useState } from "react";
+import "./services.scss";
+import DataTable from "../../components/dataTable/DataTable";
+import Add from "../../components/add/Add";
+import { GridColDef } from "@mui/x-data-grid";
+import { services } from "../../data";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "title",
+    type: "string",
+    headerName: "Serviço",
+    width: 250,
+  },
+  {
+    field: "color",
+    type: "string",
+    headerName: "Descrição",
+    width: 150,
+  },
+  {
+    field: "price",
+    type: "string",
+    headerName: "Preço",
+    width: 200,
+  },
+  
+  
+];
+
 const Services = () => {
-    return (
-        <div className="service">Serviços</div>
-    )
-    
+  const [open, setOpen] = useState(false);
+
+  // TEST THE API
+
+  // const { isLoading, data } = useQuery({
+  //   queryKey: ["allproducts"],
+  //   queryFn: () =>
+  //     fetch("http://localhost:8800/api/products").then(
+  //       (res) => res.json()
+  //     ),
+  // });
+
+  return (
+    <div className="service">
+      <div className="info">
+        <h1>Serviços</h1>
+        <button onClick={() => setOpen(true)}>Add Novo Serviço</button>
+      </div>
+      <DataTable slug="service" columns={columns} rows={services} />
+      {/* TEST THE API */}
+
+      {/* {isLoading ? (
+        "Loading..."
+      ) : (
+        <DataTable slug="products" columns={columns} rows={data} />
+      )} */}
+      {open && <Add slug="serviço" columns={columns} setOpen={setOpen} />}
+    </div>
+  );
 };
 
 export default Services;
